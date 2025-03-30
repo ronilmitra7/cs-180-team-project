6130c510a50850800cde2eb0a4171704f7cef252
ronilmitra7 <ronilmit06@gmail.com>
2025-03-30 15:03:27 -0400

    Added basic field variables and accessor and mutator methods for the User class and interface.

--- src/user/User.java
+++ src/user/User.java
@@ -1,4 +1,36 @@
 package src.user;
 
-public class User {
+public class User implements UserInterface {
+    private String username;
+    private String password;
+    private double balance;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
---
+++ src/user/UserInterface.java
@@ -0,0 +1,10 @@
+package src.user;
+
+public interface UserInterface {
+    String getUsername();
+    void setUsername(String username);
+    String getPassword();
+    void setPassword(String password);
+    double getBalance();
+    void setBalance(double balance);
+}
