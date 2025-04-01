0c285cb3769201c2f374a66884787e4ec860959b
ronilmitra7 <ronilmit06@gmail.com>
2025-04-01 14:41:55 -0400

    Started working on functionality for sending and receiving messages

--- src/Messaging/Messaging.java
+++ src/Messaging/Messaging.java
@@ -6,54 +6,18 @@ import java.util.ArrayList;
 
 public class Messaging implements MessagingInterface {
     private User sender;
-    private User recepient;
-    private String messageContent;
-    private ArrayList<String> inboxMessages;
-    private ArrayList<String> sentMessages;
 
     public Messaging(User sender) {
-        this.inboxMessages = new ArrayList<>();
-        this.sentMessages = new ArrayList<>();
         this.sender = sender;
     }
 
-    public User getSender() {
-        return sender;
+    public void sendMessage(String message, String recipient) {
+        if (message.isEmpty()) {
+            System.out.println("Can't send an empty message");
+        }
     }
 
-    public void setSender(User sender) {
-        this.sender = sender;
-    }
+    public void receiveMessage(String username) {
 
-    public User getRecepient() {
-        return recepient;
-    }
-
-    public void setRecepient(User recepient) {
-        this.recepient = recepient;
-    }
-
-    public String getMessageContent() {
-        return messageContent;
-    }
-
-    public void setMessageContent(String messageContent) {
-        this.messageContent = messageContent;
-    }
-
-    public ArrayList<String> getInboxMessages() {
-        return inboxMessages;
-    }
-
-    public void setInboxMessages(ArrayList<String> inboxMessages) {
-        this.inboxMessages = inboxMessages;
-    }
-
-    public ArrayList<String> getSentMessages() {
-        return sentMessages;
-    }
-
-    public void setSentMessages(ArrayList<String> sentMessages) {
-        this.sentMessages = sentMessages;
     }
 }
--- src/Messaging/MessagingInterface.java
+++ src/Messaging/MessagingInterface.java
@@ -5,18 +5,6 @@ import src.user.User;
 import java.util.ArrayList;
 
 public interface MessagingInterface {
-    public User getSender();
-    public void setSender(User sender);
-
-    public User getRecepient();
-    public void setRecepient(User recepient);
-
-    public String getMessageContent();
-    public void setMessageContent(String messageContent);
-
-    public ArrayList<String> getInboxMessages();
-    public void setInboxMessages(ArrayList<String> inboxMessages);
-
-    public ArrayList<String> getSentMessages();
-    public void setSentMessages(ArrayList<String> sentMessages);
+    void sendMessage(String message, String recipient);
+    void receiveMessage(String username);
 }
