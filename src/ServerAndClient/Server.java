b02603c64e2d30e595d0bb6e91ede76425f83542
ronilmitra7 <ronilmit06@gmail.com>
2025-04-09 19:51:19 -0400

    Set up server-client architecture

--- src/ServerAndClient/Server.java
+++ src/ServerAndClient/Server.java
@@ -1,5 +1,7 @@
 package ServerAndClient;
 import Database.Database;
+import user.User;
+import Messaging.Messaging;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -8,27 +10,71 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server extends Database implements Runnable, ServerInterface {
-    private ServerSocket ss;
     private Socket socket;
     private ObjectOutputStream oos;
     private ObjectInputStream ois;
 
+    public Server(Socket socket) {
+        this.socket = socket;
+    }
+
     public void run() {
         try {
-            ss = new ServerSocket(4242);
-            System.out.println("Waiting for connection...");
-            socket = ss.accept();
-            System.out.println("Accepted connection from client");
             oos = new ObjectOutputStream(socket.getOutputStream());
             ois = new ObjectInputStream(socket.getInputStream());
+
+            User user = (User) ois.readObject();
+            Database database = new Database();
+            Messaging messaging = new Messaging(user);
+
+            do {
+                String choice = (String) ois.readObject();
+
+                switch (choice) {
+                    case "1":
+                        //search user
+                        break;
+
+                    case "2":
+                        //buy item
+                        break;
+
+                    case "3":
+                        //sell item
+                        break;
+
+                    case "4":
+                        //message user
+                        break;
+
+                    case "5":
+                        //delete account
+                        break;
+
+                    default:
+                        break;
+                }
+
+            } while (true);
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 
     public static void main(String[] args) {
-        Server server = new Server();
-        Thread t = new Thread(server);
-        t.start();
+        try {
+            ServerSocket ss = new ServerSocket(4242);
+
+            while (true) {
+                Socket socket = ss.accept();
+
+                Thread thread = new Thread(new Server(socket));
+                thread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
