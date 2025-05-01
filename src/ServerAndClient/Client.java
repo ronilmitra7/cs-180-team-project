ebea99cc311ecf77b93faea24cb6e7cc34722d86
Patrick <ding398@purdue.edu>
2025-05-01 16:01:14 -0400

    .

--- src/ServerAndClient/Client.java
+++ src/ServerAndClient/Client.java
@@ -320,6 +320,7 @@ public class Client extends Database implements Runnable, ClientInterface {
         frame.getContentPane().removeAll();
         Container content = frame.getContentPane();
         content.setLayout(new BorderLayout());
+
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setBackground(new Color(0, 72, 255, 255));
         frame.setSize(800, 600);
